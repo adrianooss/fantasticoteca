1bf1e7a7d61ae22fd353786972cd8eb0a8d66b35
unknown <adrianooss@live.com>
2025-09-13 17:45:54 -0300

    Criação de Banco de Dados / Models via Django ORM - SQL  |  Adaptação do Index.html para dinamizar apresentação imagens

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html'
-                        )
+
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelecope.org / NASA / James Webb"},
+        2: {"nome": "Galáxia do Redemoinho",
+            "legenda": "telescopiovidaloka.com / NASA / Hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados}
+                            )
 
 def imagem(request):
-    return render(request, 'galeria/imagem.html'
-                        )
+    return render(request, 'galeria/imagem.html')
