2c9ffd16816dbb0bf166e98a6b07a95670ab607f
unknown <adrianooss@live.com>
2025-09-13 18:41:13 -0300

    Atualização dos arquivos index e imagem.html para buscar as imagens pelo id da fotografia via banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,16 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-
-    dados = {
-        1: {"nome": "Nebulosa de Carina",
-            "legenda": "webbtelecope.org / NASA / James Webb"},
-        2: {"nome": "Galáxia do Redemoinho",
-            "legenda": "telescopiovidaloka.com / NASA / Hubble"}
-    }
-
-    return render(request, 'galeria/index.html', {"cards": dados}
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias}
                             )
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, id=foto_id)
+    return render(request, 'galeria/imagem.html',{"fotografia": fotografia})
