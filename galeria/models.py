22a926b510d60597ce5f98b2ade7764c1bcece2b
unknown <adrianooss@live.com>
2025-09-15 19:37:32 -0300

    Configuração do modúlo Admin | Implementação do cadastro de fotografias via módulo Admin

--- galeria/models.py
+++ galeria/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 # Represeta tabela no banco de dados
 class Fotografia(models.Model):
+
+    OPCOES_CATERORIA = [
+        ("ASTROS","Astros"),
+        ("MÚSICA","Música"),
+        ("NATUREZA","Natureza"),
+        ("ESPORTES","Esportes"),
+        ("ANIMAIS","Animais"),
+        ("CIDADES","Cidades"),
+        ("PESSOAS","Pessoas"),
+        ("TECNOLOGIA","Tecnologia"),
+        ("OUTROS","Outros"),
+    ]
+        
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATERORIA ,default="")
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
