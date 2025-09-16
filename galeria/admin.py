8b42096e9b66fcbf4a501b92e681fca774f64d6e
unknown <adrianooss@live.com>
2025-09-16 19:52:16 -0300

    Inclusão dos seguintes dados no cadastro de fotografias no Admin: Data da fotografia, Publicação(boolean)

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id','nome', 'legenda')
+    list_display = ('id','nome', 'legenda','publicada','categoria')
     list_display_links = ('id','nome', 'legenda')
     search_fields = ('nome',)
     list_filter = ('categoria',)
+    list_editable = ('publicada',)
+    ordering = ('id',)
     list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Represeta tabela no banco de dados
 class Fotografia(models.Model):
@@ -20,8 +21,9 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=50, choices=OPCOES_CATERORIA ,default="")
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now,blank=False)
 
 
     def __str__(self):
         return f"Fotografia: {self.nome}"
-    
