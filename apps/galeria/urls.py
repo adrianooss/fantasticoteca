76dd9c26b2d89d9b02148f2dd8033f84ebd2550a
unknown <adrianooss@live.com>
2025-10-02 18:12:48 -0300

    Atualização das funções de filtro via tags e buscar

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.galeria.views import \
-    index, imagem, buscar, nova_imagem, editar_imagem, excluir_imagem
+    index, imagem, buscar, nova_imagem, editar_imagem, excluir_imagem, filtro
 
 urlpatterns = [
     path('', index, name='index'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('nova-imagem', nova_imagem, name='nova_imagem'),
     path('editar-imagem/<int:foto_id>', editar_imagem, name='editar_imagem'),
     path('excluir-imagem/<int:foto_id>', excluir_imagem, name='excluir_imagem'),
+    path('filtro/<str:categoria>', filtro, name='filtro'),
 ]
